4ab8e09273fa9ac9660c245fb54489984ddb7ab7
Sören Selbach <sselbach@uos.de>
2023-05-10 15:19:57 +0200

    getting it to work

--- ts_api_test/v1/modules.py
+++ ts_api_test/v1/modules.py
@@ -1,9 +1,19 @@
 import requests
+import sys
+import argparse
+
+from typing import Sequence
 
 from ts_api_test.v1 import BASE_URL
 
-def main():
-    print(BASE_URL)
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_key", nargs=1, type=str, help="API Key for the v1 API")
+    args = parser.parse_args(argv)
+
+    print(args.api_key)
+
+    return 1
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
