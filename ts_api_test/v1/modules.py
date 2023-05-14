9ef161da8f2925ab2059fd35ebef52ad299f913a
Sören Selbach <sselbach@uos.de>
2023-05-14 10:19:17 +0200

    end of pre-commit approach, switching to pytest now

--- ts_api_test/v1/modules.py
+++ ts_api_test/v1/modules.py
@@ -11,7 +11,10 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser.add_argument("api_key_file", nargs=1, type=str, help="File containing the API Key (v1)")
     args = parser.parse_args(argv)
 
-    print(args.api_key)
+    with open(args.api_key_file[0]) as f:
+        api_key = f.read()
+
+    print(api_key)
 
     return 1
 
